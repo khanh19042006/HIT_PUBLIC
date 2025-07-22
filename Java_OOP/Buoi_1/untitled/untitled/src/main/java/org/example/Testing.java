747ea495cf55baaae724b009d9c296791e3c8ed5
khanh19042006 <dinhkhanh19042006@gmail.com>
2025-07-22 12:29:41 +0700

    nop lai bai buoi 2

---
+++ Java_OOP/Buoi_1/untitled/untitled/src/main/java/org/example/Testing.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Testing {
+    public static main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a = "DKA";
+        System.out.println(a[0]);
+    }
+
+}
