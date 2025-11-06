00258c15879bc6102840a235e7e36f6fbfc6b7bd
khanh19042006 <dinhkhanh19042006@gmail.com>
2025-11-06 21:08:35 +0700

    update

--- Python/Buoi4_KT/Bai4.py
+++ Python/Buoi4_KT/Bai4.py
@@ -1,29 +1,42 @@
 a = list(map(int, input().split()))
-print(a)
+
+maxa = 0
+newa = []
+mymap = {}
+res = []
+for i in range(10):
+    mymap[i] = 0
 
 id_start = 0
 id_end = 0
-dem = 0
-demmax = 0
-dic = {}
-listmax = []
-for i in range (10):
-    dic[i] = 0
 
-while (id_end < len(a)):
-    while (dic[a[id_end]] > 1):
-        dic[id_start] -= 1
-        id_start += 1
-        dem -= 1
+while id_end < len(a):
+  mymap[a[id_end]] += 1
+  newa.append(a[id_end])
+  # print (mymap)
+  # print (newa)
+  # print (maxa)
 
-    dic[a[id_end]] += 1
-    dem += 1
+  if (len(newa) > maxa):
+    # print (newa)
+    res = newa.copy()
+    maxa = len(res)
+    if mymap[a[id_end]] > 1:
+      res.pop(maxa-1)
+      maxa -= 1
 
-    if (dem > demmax and dic[a[id_end+1]] + 1 > 1):
-        listmax = []
-        demmax = dem
-        for i in range (id_start, id_end+1):
-            listmax.append(a[i])
+  while (mymap[a[id_end]] > 1):
+    mymap[a[id_start]] -= 1
+    id_start += 1
+    newa.pop(0)
 
-print (listmax)
-print (demmax)
+  id_end += 1
+
+if (len(newa) > maxa):
+  res = newa.copy()
+  maxa = len(res)
+  if mymap[a[id_end]] > 1:
+    res.pop(maxa-1)
+
+print (res)
+print (maxa)
